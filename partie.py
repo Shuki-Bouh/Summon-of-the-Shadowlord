013e82ac9fd6051f3db5842d3925d830d850cc13
Shuki <56173021+Shuki-Bouh@users.noreply.github.com>
2023-03-24 20:28:42 +0100

    Hop les fichiers importants !

---
+++ partie.py
@@ -0,0 +1,11 @@
+class Partie(list):
+    """dans self sera contenu les ennemis avec les joueurs (à la position prévue)"""
+    def __init__(self):
+        super().__init__()
+
+    def open_save(self):
+        pass
+
+    @staticmethod
+    def write_save():
+        pass
---
+++ personnage.py
@@ -0,0 +1,118 @@
+from abc import abstractmethod, ABCMeta
+
+class Entite(metaclass=ABCMeta):
+    """Tous les personnages qui existent dans le jeu"""
+
+    def __init__(self):
+        self.vie = 0
+        self.attaque = 0
+        self.defense = 0
+        self.mana = 0
+        self.position = 0
+        self.vitesse = 0
+
+    @abstractmethod
+    def attaque(self):
+        pass
+
+    @abstractmethod
+    def attaque_speciale(self):
+        pass
+
+    @abstractmethod
+    def deplacement(self):
+        pass
+
+    @abstractmethod
+    def mort(self):
+        pass
+
+
+
+class Ennemi(Entite):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def attaque(self):
+        pass
+
+    @abstractmethod
+    def attaque_speciale(self):
+        pass
+
+    def deplacement(self):
+        pass
+
+    def mort(self):
+        pass
+
+
+class Personnage(Entite):
+    def __init__(self):
+        super().__init__()
+        self.lvl = 0
+        self.invent = 0 # Objet item
+
+    @abstractmethod
+    def attaque(self):
+        pass
+
+    @abstractmethod
+    def attaque_speciale(self):
+        pass
+
+    def deplacement(self):
+        def interagir(self):
+            """Permet d'utiliser objet"""
+            pass
+
+    def levelup(self):
+        pass
+
+    def mort(self):
+        pass
+
+
+class Epeiste(Personnage):
+    def __init__(self):
+        super().__init__()
+
+    def attaque(self):
+        pass
+
+    def attaque_speciale(self):
+        pass
+
+
+class Garde(Personnage):
+    def __init__(self):
+        super().__init__()
+
+    def attaque(self):
+        pass
+
+    def attaque_speciale(self):
+        pass
+
+
+class Sorcier(Personnage):
+    def __init__(self):
+        super().__init__()
+
+    def attaque(self):
+        pass
+
+    def attaque_speciale(self):
+        pass
+
+
+class Druide(Personnage):
+    def __init__(self):
+        super().__init__()
+
+    def attaque(self):
+        pass
+
+    def attaque_speciale(self):
+        pass
