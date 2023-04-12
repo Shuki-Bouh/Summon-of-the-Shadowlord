0413dd6b090ddac5b15d7e3bdef6628f36f2a5d7
DavidM_ln <dmelin.jeux@gmail.com>
2023-04-12 18:34:32 +0200

    @David/aka/melinda
    Crédits opérationnels !

--- Code/Credits.py
+++ Code/Credits.py
@@ -1,4 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtCore import Qt, QTimer
 
 
 class Ui_Credits(object):
@@ -44,6 +46,45 @@ class Ui_Credits(object):
         self.horizontalLayout_3.addLayout(self.verticalLayout_2)
         MainWindow.setCentralWidget(self.centralwidget)
 
+        # Partie définition des crédits
+        self.credit_list = [
+            "CRÉDITS",
+            "",
+            "",
+            "Développement :",
+            "Cyril MAISANI",
+            "David MELIN",
+            "",
+            "Programmation :",
+            "Cyril MAISANI",
+            "David MELIN",
+            "",
+            "Musique :",
+            "Cyril MAISANI",
+            "",
+            "Graphismes :",
+            "David MELIN",
+            "",
+            "Testeurs :",
+            "Cyril MAISANI",
+            "David MELIN",
+            "",
+            "",
+            "Merci d'avoir jouer à notre jeu ! (^_^)"]
+
+        # Convertit la liste de crédits en un texte avec des sauts de ligne
+        self.credit_text = "\n".join(self.credit_list)
+        self.scroll_label = QLabel(self.centralwidget)
+        self.scroll_label.setAlignment(Qt.AlignCenter)
+        self.scroll_label.setGeometry(100, 100, 600, 400)
+        self.scroll_label.setText(self.credit_text)
+        self.scroll_position = 0  # position initiale de l'étiquette
+        self.scroll_defilement = True  # permet de stopper le défilement
+
+        self.timer = QTimer(self.centralwidget)
+        self.timer.timeout.connect(self.scroll_credit)
+        self.timer.start(24)  # ajuster l'intervalle de temps en fonction de vos besoins
+
         Ui_Credits.list_widgets.append([self.horizontalLayout_2, 1])
         Ui_Credits.list_widgets.append([self.horizontalLayout_3, 2])
         Ui_Credits.list_widgets.append([self.verticalLayout_2, 3])
@@ -52,6 +93,12 @@ class Ui_Credits(object):
         Ui_Credits.list_widgets.append([self.widget_3, 6])
         Ui_Credits.list_widgets.append([self.centralwidget, 7])
         Ui_Credits.list_widgets.append([self.bouton_menu, 8])
+        Ui_Credits.list_widgets.append([self.credit_text, 9])
+        Ui_Credits.list_widgets.append([self.credit_list, 10])
+        Ui_Credits.list_widgets.append([self.scroll_label, 11])
+        Ui_Credits.list_widgets.append([self.scroll_position, 12])
+        Ui_Credits.list_widgets.append([self.scroll_defilement, 13])
+        Ui_Credits.list_widgets.append([self.timer, 14])
 
         self.retranslate_Cred(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -60,6 +107,14 @@ class Ui_Credits(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Summon_of_the_ShadowLand"))
         self.bouton_menu.setText(_translate("MainWindow", "Menu"))
+
+    def scroll_credit(self):
+        if self.scroll_defilement:
+            self.scroll_position -= 1  # décrémente la position de défilement
+            self.scroll_label.move(100, 100 + self.scroll_position)  # met à jour la position de l'étiquette
+
+            if self.scroll_position <= -450:  # ajuster la limite inférieure en fonction de vos besoins
+                self.scroll_defilement = False  # réinitialise la position de défilement
 
     def fermer(self):
         for widget in Ui_Credits.list_widgets:
